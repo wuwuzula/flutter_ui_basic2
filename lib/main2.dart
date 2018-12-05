e5037dcc75034084445af8b35f3136c3e27b9d39
androidmalin <malin.myemail@163.com>
2018-12-05 17:25:32 +0800

    add listview

--- lib/main2.dart
+++ lib/main2.dart
@@ -5,57 +5,52 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     final buildings = [
-      Building(
-          BuildingType.theater, 'CineArts at the Empire', '85 W Portal Ave'),
+      Building(BuildingType.theater, 'CineArts at the Empire', '85 W Portal Ave'),
       Building(BuildingType.theater, 'The Castro Theater', '429 Castro St'),
-      Building(
-          BuildingType.theater, 'Alamo Drafthouse Cinema', '2550 Mission St'),
+      Building(BuildingType.theater, 'Alamo Drafthouse Cinema', '2550 Mission St'),
       Building(BuildingType.theater, 'Roxie Theater', '3117 16th St'),
-      Building(BuildingType.theater, 'United Artists Stonestown Twin',
-          '501 Buckingham Way'),
+      Building(BuildingType.theater, 'United Artists Stonestown Twin', '501 Buckingham Way'),
       Building(BuildingType.theater, 'AMC Metreon 16', '135 4th St #3000'),
       Building(BuildingType.restaurant, 'K\'s Kitchen', '1923 Ocean Ave'),
-      Building(BuildingType.restaurant, 'Chaiya Thai Restaurant',
-          '72 Claremont Blvd'),
+      Building(BuildingType.restaurant, 'Chaiya Thai Restaurant', '72 Claremont Blvd'),
       Building(BuildingType.restaurant, 'La Ciccia', '291 30th St'),
 
       // double 一下
-      Building(
-          BuildingType.theater, 'CineArts at the Empire', '85 W Portal Ave'),
+      Building(BuildingType.theater, 'CineArts at the Empire', '85 W Portal Ave'),
       Building(BuildingType.theater, 'The Castro Theater', '429 Castro St'),
-      Building(
-          BuildingType.theater, 'Alamo Drafthouse Cinema', '2550 Mission St'),
+      Building(BuildingType.theater, 'Alamo Drafthouse Cinema', '2550 Mission St'),
       Building(BuildingType.theater, 'Roxie Theater', '3117 16th St'),
-      Building(BuildingType.theater, 'United Artists Stonestown Twin',
-          '501 Buckingham Way'),
+      Building(BuildingType.theater, 'United Artists Stonestown Twin', '501 Buckingham Way'),
       Building(BuildingType.theater, 'AMC Metreon 16', '135 4th St #3000'),
       Building(BuildingType.restaurant, 'K\'s Kitchen', '1923 Ocean Ave'),
-      Building(BuildingType.restaurant, 'Chaiya Thai Restaurant',
-          '72 Claremont Blvd'),
+      Building(BuildingType.restaurant, 'Chaiya Thai Restaurant', '72 Claremont Blvd'),
       Building(BuildingType.restaurant, 'La Ciccia', '291 30th St'),
     ];
     return MaterialApp(
       title: 'ListView demo',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Buildings'),
-          ),
-          body: BuildingListView(
-              buildings, (index) => debugPrint('item $index clicked'))),
+        appBar: AppBar(
+          title: Text('Buildings'),
+        ),
+        body: BuildingListView(buildings, (index) => debugPrint('item $index clicked'))
+      ),
     );
   }
 }
 
-///  bean
+
+
 enum BuildingType { theater, restaurant }
 
 class Building {
-  BuildingType type;
-  String title;
-  String address;
+  final BuildingType type;
+  final String title;
+  final String address;
+
   Building(this.type, this.title, this.address);
 }
 
@@ -71,7 +66,9 @@ class ItemView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final icon = Icon(
-        building.type == BuildingType.restaurant ? Icons.radio : Icons.theaters,
+        building.type == BuildingType.restaurant
+            ? Icons.restaurant
+            : Icons.theaters,
         color: Colors.blue[500]);
 
     final widget = Row(
@@ -86,8 +83,8 @@ class ItemView extends StatelessWidget {
             children: <Widget>[
               Text(building.title,
                   style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
                   )),
               Text(building.address)
             ],
@@ -96,7 +93,10 @@ class ItemView extends StatelessWidget {
       ],
     );
 
-    return InkWell(onTap: () => listener(position), child: widget);
+    return InkWell(
+      onTap: () => listener(position),
+      child: widget
+    );
   }
 }
 
@@ -108,12 +108,11 @@ class BuildingListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// ListView.builder 可以按需生成子控件
     return ListView.builder(
       itemCount: buildings.length,
       itemBuilder: (context, index) {
         return new ItemView(index, buildings[index], listener);
-      },
+      }
     );
   }
 }
